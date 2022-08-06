a78f5211e5c6b2391b3c1b5a4323db36b132396b
Aaron Kollasch <aaron@kollasch.dev>
2022-08-06 18:23:48 -0400

    Update jitouch from 2.80 to 2.81

--- Casks/jitouch.rb
+++ Casks/jitouch.rb
@@ -1,6 +1,6 @@
 cask "jitouch" do
-  version "2.80"
-  sha256 "33d183e9a31015d6457e07bb37ad747a58d21a10d8ee032e721014d28b07ae01"
+  version "2.81"
+  sha256 "b2205079633d96b5e7276fcbe9274ce50c17a806992731f45c560eca33ea2afd"
 
   url "https://github.com/aaronkollasch/jitouch/releases/download/v#{version}/Install-Jitouch.pkg"
   name "jitouch"
@@ -9,13 +9,13 @@ cask "jitouch" do
 
   pkg "Install-Jitouch.pkg"
 
-  uninstall pkgutil: "com.jitouch.Jitouch.*",
+  uninstall pkgutil:   "com.jitouch.Jitouch.*",
             launchctl: "com.jitouch.Jitouch.agent"
 
   zap trash: [
     "~/Library/Preferences/com.jitouch.Jitouch.plist",
     "~/Library/LaunchAgents/com.jitouch.Jitouch.plist",
     "~/Library/Logs/com.jitouch.Jitouch.log",
-    "~/Library/Logs/com.jitouch.Jitouch.prefpane.log"
+    "~/Library/Logs/com.jitouch.Jitouch.prefpane.log",
   ]
 end
