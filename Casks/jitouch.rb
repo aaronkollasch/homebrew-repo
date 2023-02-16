bc17b5db187c04b5042944f1bb115d98c7def5b7
Aaron Kollasch <aaron@kollasch.dev>
2023-02-16 11:12:13 -0500

    Update Jitouch cask to JitouchApp

--- Casks/jitouch.rb
+++ Casks/jitouch.rb
@@ -2,14 +2,18 @@ cask "jitouch" do
   version "2.82.1"
   sha256 "3f5194a4da6fe19d17c843fa8a876131f7878905dcbb2e1d740d34d286d740c4"
 
-  url "https://github.com/aaronkollasch/jitouch/releases/download/v#{version}/Install-Jitouch.pkg"
-  name "jitouch"
+  url "https://github.com/JitouchApp/Jitouch/releases/download/v#{version}/Install-Jitouch.pkg",
+      verified: "github.com/JitouchApp/Jitouch/"
+  name "Jitouch"
   desc "Multi-touch gestures editor"
-  homepage "https://github.com/aaronkollasch/jitouch"
+  homepage "https://www.jitouch.com/"
+
+  depends_on macos: ">= :high_sierra"
 
   pkg "Install-Jitouch.pkg"
 
-  uninstall pkgutil:   "com.jitouch.Jitouch.*",
+  uninstall quit:      "com.jitouch.Jitouch",
+            pkgutil:   "com.jitouch.Jitouch",
             launchctl: "com.jitouch.Jitouch.agent"
 
   zap trash: [
